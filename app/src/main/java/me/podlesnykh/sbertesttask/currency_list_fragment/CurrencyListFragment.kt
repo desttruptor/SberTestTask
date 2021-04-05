ce2def368ba66c3c2a35cec1b43d1cc852bc7922
desttruptor <41233120+desttruptor@users.noreply.github.com>
2021-04-05 18:52:39 +0300

    added recycler view adapter class

--- app/src/main/java/me/podlesnykh/sbertesttask/currency_list_fragment/CurrencyListFragment.kt
+++ app/src/main/java/me/podlesnykh/sbertesttask/currency_list_fragment/CurrencyListFragment.kt
@@ -5,24 +5,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.FragmentNavigatorDestinationBuilder
 import androidx.navigation.fragment.findNavController
-import me.podlesnykh.sbertesttask.currency_converter_fragment.CurrencyConverterFragment
 import me.podlesnykh.sbertesttask.databinding.FragmentCurrencyListBinding
+import me.podlesnykh.sbertesttask.network.CurrencyItem
 
 class CurrencyListFragment : Fragment() {
 
     private var _binding: FragmentCurrencyListBinding? = null
     private val binding get() = _binding!!
 
+    // адаптер инициализируется пустым списком
+    private val currencyList: List<CurrencyItem> = emptyList()
+    private val adapter = CurrencyListAdapter(currencyList, this::openConverterScreen)
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        super.onCreateView(inflater, container, savedInstanceState)
         _binding = FragmentCurrencyListBinding.inflate(inflater, container, false)
         val view = binding.root
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
     }
 
     override fun onDestroyView() {
@@ -30,9 +39,14 @@ class CurrencyListFragment : Fragment() {
         _binding = null
     }
 
+    // переход на экран конвертера выбранной валюты
     private fun openConverterScreen(numCode: Int) {
         findNavController().navigate(
             CurrencyListFragmentDirections.navigateToConverter(numCode)
         )
+    }
+
+    private fun setupRecyclerView() {
+        binding.rvCurrencyList.adapter = adapter
     }
 }
